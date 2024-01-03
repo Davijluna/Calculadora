e0cf2b9d73e438c297902ec2093277c500312b11
davi <davijluna@gmail.com>
2024-01-03 00:54:33 -0300

    feat: adicionando um teste com finalidade de estudo

--- script.js
+++ script.js
@@ -1,11 +1,75 @@
-// let input = document.querySelector('.inputCalc')
-function insert(number) {
-  if(number) {
-    document.querySelector('.inputCalc').innerHTML = number
-  }
-    document.querySelector('.inpinputCalc').innerHTML += number
-}
-let button = document.getElementsByClassName('.number')
+// // let input = document.querySelector('.inputCalc')
+// // function insert(number) {
+// //   if(number) {
+// //     document.querySelector('.inputCalc').innerHTML = number
+// //   }
+// //     document.querySelector('.inpinputCalc').innerHTML += number
+// // }
+// // let button = document.getElementsByClassName('.number')
 
-console.log(button)
+// // console.log(button)
+
+// let number = 0
+
+
+// const soma = (numer1, numer2) => {
+//   console.log(numer1 + numer2)
+//   return numer1 + numer2
+// }
+
+// const sub = (numer1, numer2) => {
+//   console.log(numer1 - numer2)
+//   return numer1 - numer2
+// }
+
+// const div = (numer1, numer2) => {
+//   console.log(numer1 / numer2)
+//   return numer1 / numer2
+// }
+
+// const mult = (numer1, numer2) => {
+//   console.log(numer1 * numer2)
+//   return numer1 * numer2
+// }
+
+// const selectOperation = (numer1, sinal, numer2 ) => {
+//   if (sinal === '+') return soma(numer1, numer2)
+//   if (sinal === '-') return sub(numer1, numer2)
+//   if (sinal === '/') return div(numer1, numer2)
+//   if (sinal === '*') return mult(numer1, numer2)
+// }
+
+// selectOperation(5, '-', 5)
+
+let displayValue = "";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = "";
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue).toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = "Error";
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById("display").value = displayValue;
+}
+
+
+
+
+
+
 
