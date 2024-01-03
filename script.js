f3d4895f7bf8a2134ea8b5e793a431eb384eb607
davi <davijluna@gmail.com>
2024-01-03 00:11:39 -0300

    feat: adicionando numero no input

---
+++ script.js
@@ -0,0 +1,11 @@
+// let input = document.querySelector('.inputCalc')
+function insert(number) {
+  if(number) {
+    document.querySelector('.inputCalc').innerHTML = number
+  }
+    document.querySelector('.inpinputCalc').innerHTML += number
+}
+let button = document.getElementsByClassName('.number')
+
+console.log(button)
+
