8101dd42f3b48d8e8cc0dc494a84c9430be0f862
davi <davijluna@gmail.com>
2024-01-05 22:08:29 -0300

    feat: adicionando alterações

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// // let input = document.querySelector('.inputCalc')
-// // function insert(number) {
-// //   if(number) {
-// //     document.querySelector('.inputCalc').innerHTML = number
-// //   }
-// //     document.querySelector('.inpinputCalc').innerHTML += number
-// // }
-// // let button = document.getElementsByClassName('.number')
+function insert(number) {
 
-// // console.log(button)
+  let input = document.querySelector('.inputCalc').innerText
+  
+    document.querySelector('.inputCalc').innerHTML = input + number
+}
+let button = document.getElementsByClassName('.number')
+
+// console.log(button)
 
 // let number = 0
 
@@ -39,34 +38,7 @@
 //   if (sinal === '*') return mult(numer1, numer2)
 // }
 
-// selectOperation(5, '-', 5)
-
-let displayValue = "";
-
-function appendToDisplay(value) {
-  displayValue += value;
-  updateDisplay();
-}
-
-function clearDisplay() {
-  displayValue = "";
-  updateDisplay();
-}
-
-function calculate() {
-  try {
-    displayValue = eval(displayValue).toString();
-    updateDisplay();
-  } catch (error) {
-    displayValue = "Error";
-    updateDisplay();
-  }
-}
-
-function updateDisplay() {
-  document.getElementById("display").value = displayValue;
-}
-
+// selectOperation(5, '*', 5)
 
 
 
